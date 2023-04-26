52e1cafc698447a2c1c1179f324ee45d4b47ffd9
John <johnathan.bizzano@gmail.com>
2023-04-26 17:08:09 -0400

    1.6

--- gui_remote.jl
+++ gui_remote.jl
@@ -7,7 +7,26 @@ scriptname = ARGS[2]
 make_sys_image = parse(Bool, ARGS[3])
 
 if make_sys_image
-   eval(quote using JuliaSAILGUI end)
+   eval(quote 
+        using JuliaSAILGUI, Dates, DataFrames, Gtk, CairoMakie, Makie, Observables, CSV 
+
+        fig = Figure()
+        ax = Axis(fig[1, 1])
+        d = DataFrame(test=>[1, 2, 3], test2=>[1, 2, 3])
+        lines!(ax, Observable(d.test), Observable(d.test2))
+
+        win = GtkWindow()
+        box = GtkBox()
+        push!(win, box)
+        can = GtkCanvas(200, 200)
+        push!(box, can)
+        makie_draw(can, fig)
+
+        @async showall(win)
+        @async Gtk.gtk_main()
+
+        makie_draw(plotCanvasObject, fig)
+    end)
    create_sysimage(nothing; sysimage_path=dllname)
 end 
     
