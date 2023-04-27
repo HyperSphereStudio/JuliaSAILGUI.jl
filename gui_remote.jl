4c08debfd0d5bbc425dba36fa7a1e7975397f90a
John <johnathan.bizzano@gmail.com>
2023-04-26 21:14:49 -0400

    Version 1.7

--- gui_remote.jl
+++ gui_remote.jl
@@ -7,27 +7,12 @@ scriptname = ARGS[2]
 make_sys_image = parse(Bool, ARGS[3])
 
 if make_sys_image
-   eval(quote 
-        using JuliaSAILGUI, Dates, DataFrames, Gtk, CairoMakie, Makie, Observables, CSV 
-
-        fig = Figure()
-        ax = Axis(fig[1, 1])
-        d = DataFrame(test=>[1, 2, 3], test2=>[1, 2, 3])
-        lines!(ax, Observable(d.test), Observable(d.test2))
-
-        win = GtkWindow()
-        box = GtkBox(:v)
-        push!(win, box)
-        can = GtkCanvas(200, 200)
-        push!(box, can)
-        makie_draw(can, fig)
-
-        @async showall(win)
-        @async Gtk.gtk_main()
-
-        makie_draw(can, fig)
-    end)
-   create_sysimage(nothing; sysimage_path=dllname)
+   modules = eval(quote 
+            using JuliaSAILGUI 
+            JuliaSAILGUI.run_test() 
+            return [ccall(:jl_module_usings, Any, (Any,), @__MODULE__)..., @__MODULE__]
+        end) 
+   create_sysimage(map(nameof, modules); sysimage_path=dllname)
 end 
     
 open("gui.bat", "w") do f
--- src/JuliaSAILGUI.jl
+++ src/JuliaSAILGUI.jl
@@ -1,6 +1,5 @@
 module JuliaSAILGUI
-    using Gtk, CairoMakie, Makie, Observables
-    using Dates, DataFrames
+    using Gtk, CairoMakie, Makie, Observables, CSV, Dates, DataFrames, LibSerialPort
 
     export gui_main, makie_draw, gtk_fixed_move, gtk_to_string
 
@@ -22,4 +21,25 @@ module JuliaSAILGUI
     gtk_fixed_move(fixed, widget, x, y) = ccall((:gtk_fixed_move, Gtk.libgtk), Nothing, (Ptr{GObject}, Ptr{GObject}, Cint, Cint), fixed, widget, x, y)
     
     gtk_to_string(s) = s == C_NULL ? "" : Gtk.bytestring(s)
+
+    function run_test()
+        @eval(@__MODULE__, quote 
+            fig = Figure()
+            ax = Axis(fig[1, 1])
+            d = DataFrame(test=[1, 2, 3], test2=[1, 2, 3])
+            lines!(ax, Observable(d.test), Observable(d.test2))
+
+            win = GtkWindow()
+            box = GtkBox(:v)
+            push!(win, box)
+            can = GtkCanvas(200, 200)
+            push!(box, can)
+            makie_draw(can, fig)
+
+            @async showall(win)
+            @async Gtk.gtk_main()
+
+            makie_draw(can, fig)
+        end)
+    end
 end
