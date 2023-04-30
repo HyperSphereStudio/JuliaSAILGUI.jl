eed77a6cc089ec88f5fe91bcfb2338662eb97bd8
John <johnathan.bizzano@gmail.com>
2023-04-30 15:14:38 -0400

    Version 1.9

--- src/JuliaSAILGUI.jl
+++ src/JuliaSAILGUI.jl
@@ -1,22 +1,9 @@
 module JuliaSAILGUI
-    using Gtk, CairoMakie, Observables, CSV, Dates, DataFrames, LibSerialPort
+    using Gtk4, GLMakie, Observables, CSV, Dates, DataFrames, LibSerialPort
 
     export gui_main, makie_draw, gtk_fixed_move, gtk_to_string
 
     include("MicroControllerPort.jl")
-
-    CairoMakie.activate!()
-
-    function makie_draw(canvas, fig)
-        @guarded draw(canvas) do _
-           scene = fig.scene
-           resize!(scene, Gtk.width(canvas), Gtk.height(canvas))
-           config = CairoMakie.ScreenConfig(1.0, 1.0, :good, true, true)
-           screen = CairoMakie.Screen(scene, config, Gtk.cairo_surface(canvas))
-           CairoMakie.cairo_draw(screen, scene)
-        end
-        show(canvas)
-    end
     
     gtk_fixed_move(fixed, widget, x, y) = ccall((:gtk_fixed_move, Gtk.libgtk), Nothing, (Ptr{GObject}, Ptr{GObject}, Cint, Cint), fixed, widget, x, y)
     
@@ -34,12 +21,9 @@ module JuliaSAILGUI
             push!(win, box)
             can = GtkCanvas(200, 200)
             push!(box, can)
-            makie_draw(can, fig)
 
             @async showall(win)
             @async Gtk.gtk_main()
-
-            makie_draw(can, fig)
         end)
     end
 end
