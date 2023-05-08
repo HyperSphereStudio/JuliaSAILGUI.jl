b1c56a783759f119d017fdf9a3976b7d7407472d
John <johnathan.bizzano@gmail.com>
2023-05-07 23:03:29 -0400

    Update JuliaSAILGUI.jl

--- src/JuliaSAILGUI.jl
+++ src/JuliaSAILGUI.jl
@@ -6,7 +6,7 @@ module JuliaSAILGUI
     using Gtk4.GLib: GObject, signal_handler_is_connected, signal_handler_disconnect
     using GLMakie.GLAbstraction
     using GLMakie.Makie
-    using GLMakie: empty_postprocessor, fxaa_postprocessor, OIT_postprocessor, to_screen_postprocessor
+    using GLMakie: empty_postprocessor, fxaa_postprocessor, to_screen_postprocessor
     using GLMakie.Makie: MouseButtonEvent, KeyEvent
 
     include("MicroControllerPort.jl")
