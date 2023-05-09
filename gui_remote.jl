9653165dc097a4596229c6867446523a0b9cdc1c
John <johnathan.bizzano@gmail.com>
2023-05-09 00:41:16 -0400

    Update gui_remote.jl

--- gui_remote.jl
+++ gui_remote.jl
@@ -16,9 +16,9 @@ Pkg.add(url="https://github.com/HyperSphereStudio/JuliaSAILGUI.jl")
 
 dllname = joinpath(Sys.BINDIR, ARGS[1])
 scriptname = ARGS[2]
-make_sys_image = parse(Bool, ARGS[3])
 
-if make_sys_image
+println("Compile System Image [true/false]?")
+if parse(Bool, readline())
    println("Warming environment...") 
    modules = eval(
         quote 
@@ -30,11 +30,12 @@ if make_sys_image
             return [ccall(:jl_module_usings, Any, (Any,), @__MODULE__)..., @__MODULE__]
         end) 
    invalid_modules = [Base, Main, Core]  
-   println("Compiling Image...")   
-   create_sysimage(map(nameof, filter(m -> !(m in invalid_modules), modules)); sysimage_path=dllname)
+   compiling_modules = map(nameof, filter(m -> !(m in invalid_modules), modules))
+   println("Compiling Image containing $compiling_modules to $dllname")   
+   create_sysimage(compiling_modules; sysimage_path=dllname)
 end 
     
-println("Created file \"gui.bat\"")
+println("Creating System Image Executable File \"gui.bat\"")
 open("gui.bat", "w") do f
     write(f, "julia --sysimage $dllname $scriptname")
 end
