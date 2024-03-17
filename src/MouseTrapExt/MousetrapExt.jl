30a4961210c900220c67732ef5a458b1229cafe5
John <johnathan.bizzano@gmail.com>
2024-03-16 23:44:06 -0400

    Version 6.2

--- src/MouseTrapExt/MousetrapExt.jl
+++ src/MouseTrapExt/MousetrapExt.jl
@@ -16,3 +16,12 @@ macro SetMousetrapProperties(x, args...)
 
     return block
 end
+
+function Base.empty!(ax::Axis3)
+    while !isempty(ax.scene.plots)
+        delete!(ax.scene, ax.scene.plots[end])
+    end
+end
+
+Mousetrap.@export_function ColumnView set_show_row_separators! Cvoid Bool b
+Mousetrap.@export_function ColumnView set_show_column_separators! Cvoid Bool b
--- src/MouseTrapExt/Observables.jl
+++ src/MouseTrapExt/Observables.jl
@@ -26,7 +26,7 @@ end
 
 Base.getindex(w::Scale) = get_value(w)
 Base.setindex!(w::Scale, x::Number) = set_value!(w, x)
-Observables.on(f, w::Scale) = connect_signal_value_changed!(x -> (f(x[]); return nothing), w)
+Observables.on(f, w::Scale) = connect_signal_value_changed!(x -> (f(w[]); return nothing), w)
 
 Base.getindex(w::Label) = get_text(w)
 Base.setindex!(w::Label, x::String) = set_text!(w, x)
@@ -34,19 +34,19 @@ Observables.on(f, w::Label) = ()
 
 Base.getindex(w::Button) = false
 Base.setindex!(w::Button, x::Bool) = ()
-Observables.on(f, w::Button) = connect_signal_clicked!(x -> (f(x); return nothing), w)
+Observables.on(f, w::Button) = connect_signal_clicked!(x -> (f(w); return nothing), w)
 
 Base.getindex(w::ToggleButton) = get_is_active(w)
 Base.setindex!(w::ToggleButton, x::Bool) = set_is_active!(w, x)
-Observables.on(f, w::ToggleButton) = connect_signal_toggled!(x -> (f(x); return nothing), w)
+Observables.on(f, w::ToggleButton) = connect_signal_toggled!(x -> (f(w); return nothing), w)
 
 Base.getindex(w::Switch) = get_is_active(w)
 Base.setindex!(w::Switch, x::Bool) = set_is_active!(w, x)
-Observables.on(f, w::Switch) = connect_signal_switched!(x -> (f(x); return nothing), w)
+Observables.on(f, w::Switch) = connect_signal_switched!(x -> (f(w); return nothing), w)
 
 Base.getindex(w::SpinButton) = get_value(w)
 Base.setindex!(w::SpinButton, x::Number) = set_value!(w, x)
-Observables.on(f, w::SpinButton) = connect_signal_value_changed!(x -> (f(x[]); return nothing), w)
+Observables.on(f, w::SpinButton) = connect_signal_value_changed!(x -> (f(w[]); return nothing), w)
 
 Base.getindex(w::LevelBar) = get_value(w)
 Base.setindex!(w::LevelBar, x::Number) = set_value!(w, x)
@@ -62,5 +62,5 @@ Observables.on(f, w::Spinner) = ()
 
 Base.getindex(w::Entry) = get_text(w)
 Base.setindex!(w::Entry, x::String) = set_text!(w, x)
-Observables.on(f, w::Entry) = connect_signal_activate!(x -> (f(x[]); return nothing), w)
+Observables.on(f, w::Entry) = connect_signal_activate!(x -> (f(w[]); return nothing), w)
 
