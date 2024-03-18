de896847874de5f88426359ad1377a42a5615115
John <johnathan.bizzano@gmail.com>
2024-03-18 13:52:37 -0400

    Version 6.4

--- src/JuliaSAILGUI.jl
+++ src/JuliaSAILGUI.jl
@@ -2,6 +2,8 @@ module JuliaSAILGUI
 	using Reexport
 	@reexport using GLMakie, Observables, LibSerialPort, HTTP, FileIO, GeometryBasics
 	using GLMakie, Observables, LibSerialPort, FileIO, PrecompileTools, DataFrames
+	
+	export dict
 
     include("MouseTrapExt/MousetrapExt.jl")
     include("MicroControllerPort.jl")
@@ -11,6 +13,22 @@ module JuliaSAILGUI
 	
 	Base.isopen(::Nothing) = false
     Base.append!(d::Dict, items::Pair...) = foreach(p -> d[p[1]] = p[2], items)
+	dict(; values...) = Dict(values...)
+
+	struct Expando{T}
+		dict::Dict{Symbol, T}
+
+		Expando{T}(; args...) where T = new{T}(Dict{Symbol, T}(collect(args)))
+		Expando{T}() where T = new{T}(Dict{Symbol, T}())
+		Expando() = Expando{Any}()
+
+		Base.getproperty(x::Expando, s::Symbol) = getfield(x, :dict)[s]
+		Base.setproperty!(x::Expando, s::Symbol, v) = getfield(x, :dict)[s] = v
+		Base.delete!(x::Expando, s) = delete!(Dict(x), s)
+		Base.keys(x::Expando) = keys(Dict(x))
+		Base.values(x::Expando) = values(Dict(x))
+		Base.Dict(x::Expando) = x.dict
+	end 
 
     function __init__()
         init_ports()
