cd73a6de4ac037108715b392bf9e3b34189324e2
John <johnathan.bizzano@gmail.com>
2024-03-28 20:50:41 -0400

    Version 6.1

--- src/MouseTrapExt/MousetrapExt.jl
+++ src/MouseTrapExt/MousetrapExt.jl
@@ -23,34 +23,42 @@ function Base.empty!(ax::Axis3)
     end
 end
 
-using Glib_jll
-
 const JuliaReferences = Tuple{Function, Function}[]
-
+	
 julia_create_ref(f, on_error_function) = (push!(JuliaReferences, (f, on_error_function)); length(JuliaReferences))
-const julia_src_function = @cfunction(
-    function src_function(x)
+function src_function(x)
+    try
+        return Cint(JuliaReferences[x][1]())
+    catch e
         try
-            return Cint(JuliaReferences[x][1]())
-        catch e
-            try
-                JuliaReferences[x][2](e)
-            catch e2
-                display_error(e2)
-            end
-            return Cint(false)
+            JuliaReferences[x][2](e)
+        catch e2
+            display_error(e2)
         end
-    end, Cint, (Cint, ))
-const julia_destroy_create_ref_c = @cfunction(
-    function julia_destroy_ref(x)
-        deleteat!(JuliaReferences, x)
-        return nothing
-    end, Cvoid, (Cint, ))
+        return Cint(false)
+    end
+end
+
+function julia_destroy_ref(x)
+    deleteat!(JuliaReferences, x)
+    return nothing
+end
 
 display_error(e) = showerror(stdout, e, catch_backtrace())    
-function g_timeout_add(f::Function, interval::Integer; on_error_function::Function = display_error)
-    ccall((:g_timeout_add_full, libglib), Cint, (Cint, UInt32, Ptr{Cvoid}, Cint, Ptr{Cvoid}), Int32(0), UInt32(interval), julia_src_function, julia_create_ref(f, on_error_function), julia_destroy_create_ref_c)
-end
-function g_idle_add(f::Function; on_error_function::Function = display_error)
-    ccall((:g_idle_add_full, libglib), Cint, (Cint, Ptr{Cvoid}, Cint, Ptr{Cvoid}), Int32(0), julia_src_function, julia_create_ref(f, on_error_function), julia_destroy_create_ref_c)
+
+function init_mousetrap()
+	@eval quote
+		using Glib_jll
+		
+		const julia_src_function = @cfunction($src_function, Cint, (Cint, ))
+		const julia_destroy_create_ref_c = @cfunction($julia_destroy_ref, Cvoid, (Cint, ))
+		
+		function g_timeout_add(f::Function, interval::Integer; on_error_function::Function = display_error)
+			ccall((:g_timeout_add_full, libglib), Cint, (Cint, UInt32, Ptr{Cvoid}, Cint, Ptr{Cvoid}), Int32(0), UInt32(interval), julia_src_function, julia_create_ref(f, on_error_function), julia_destroy_create_ref_c)
+		end
+		
+		function g_idle_add(f::Function; on_error_function::Function = display_error)
+			ccall((:g_idle_add_full, libglib), Cint, (Cint, Ptr{Cvoid}, Cint, Ptr{Cvoid}), Int32(0), julia_src_function, julia_create_ref(f, on_error_function), julia_destroy_create_ref_c)
+		end
+	end
 end
